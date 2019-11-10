5d0dc6699b8f2e5bfd80eb2fc278f372f3724946
Jordan Prazak <jordan.prazak@yahoo.com>
2019-11-10 08:12:42 -0800

    Add files via upload
    Edited config parameters

--- Realsense_Camera_Part.py
+++ Realsense_Camera_Part.py
@@ -7,19 +7,18 @@ Created on Tue Nov  5 22:41:53 2019
 
 import cv2
 import time
-from pyrealsense2 import pipeline
+from pyrealsense2 import pipeline,config,stream
 import numpy as np
 
 
 
 class RealsenseCameraPart(object):
 
-    def __init__(self, image_w=1280, image_h=720, image_d=4,framerate=30):
+    def __init__(self, image_w=640, image_h=480, image_d=4):
         self.combined_array = None
         self.running = True
         self.image_w = image_w
         self.image_h = image_h
-        self.framerate = framerate
         self.depth, self.color = None, None
 
         # Set stream speed and resolution
@@ -29,37 +28,9 @@ class RealsenseCameraPart(object):
         self.pipe = pipeline()
         #load the configuration
         cfg = config()
-        cfg.enable_stream(stream.color,stream_index=-1,width=image_w,height=image_h,framerate=framerate)
-        cfg.enable_stream(stream.depth, stream_index=-1,width=image_w,height=image_h,framerate=framerate)
+        cfg.enable_stream(stream.color,stream_index=-1,width=640,height=480,framerate=30)
+        cfg.enable_stream(stream.depth, stream_index=-1,width=640,height=480,framerate=30)
         self.pipe.start(cfg)
-        
-#    def get_depth(self):
-#        """
-#        Returns numpy ndarrays representing the raw and ranged depth images.
-#        Outputs:
-#            dmap: distance map in mm, 1L ndarray, dtype=uint16, min=0, max=2**12-1
-#            d4d:  depth for display, 3L ndarray, dtype=uint8, min=0, max=255
-#        """
-#        dmap = np.frombuffer(self.depth_stream.read_frame().get_buffer_as_uint16(), dtype=np.uint16).reshape(self.image_h, self.image_w)
-#        d4d = np.uint8(dmap.astype(float) * 255 / 2 ** 12 - 1)
-#        d4d = cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
-#        # d4d = np.dstack((d4d, d4d, d4d))
-#
-#        return dmap, d4d
-
-#    def get_color(self):
-#        """
-#        Returns numpy ndarrays representing raw and ranged infra-red(IR) images.
-#        outputs:
-#            ir  : raw IR, 1L ndarrya, dtype=uint16, min=0, max=2**12-1
-#            ir4d: IR for display, 3L ndarray, dtype=uint8, min=0, max=255
-#        """
-#        ir_frame = self.ir_stream.read_frame()
-#        ir_frame_data = self.ir_stream.read_frame().get_buffer_as_uint16()
-#        ir4d = np.ndarray((ir_frame.height, ir_frame.width), dtype=np.uint16, buffer=ir_frame_data).astype(np.float32)
-#        ir4d = np.uint8((ir4d / ir4d.max()) * 255)
-#        ir4d = cv2.cvtColor(ir4d, cv2.COLOR_GRAY2RGB)
-#        return ir_frame, ir4d
 
     def poll(self):
         frame_data = self.pipe.wait_for_frames()
